d18cac53b9d3082dbe1465349f3b683df25c75b0
Aleksandra Szewczyk <aleksandra_szewczyk@o2.pl>
2017-03-13 22:18:35 +0100

    Changed Column.js

--- Column.js
+++ Column.js
@@ -1,12 +1,12 @@
 function Column(id, name) {
-    var self = this;
+	var self = this;
 	
 	this.id = id;
 	this.name = name || 'Nie podano nazwy';
 	this.element = createColumn();
 
-    function createColumn() {
-		// TWORZENIE NOWYCH WĘZŁÓW
+	function createColumn() {
+        // TWORZENIE NOWYCH WĘZŁÓW
 		var column = $('<div class="column"></div>');
 		var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
 		var columnCardList = $('<ul class="card-list"></ul>');
@@ -19,30 +19,31 @@ function Column(id, name) {
 		});
 		
         columnAddCard.click(function(event) {
-	       var cardName = prompt("Wpisz nazwę karty");
-	       event.preventDefault();
-	       $.ajax({
-               url: baseUrl + '/card',
-               method: 'POST',
-               data: {
-                   name: cardName,
-                   bootcamp_kanban_column_id: self.id
+            var cardName = prompt("Wpisz nazwę karty");
+            event.preventDefault();
+            $.ajax({
+                url: baseUrl + '/card',
+                method: 'POST',
+                data: {
+                    name: cardName,
+                    bootcamp_kanban_column_id: self.id
                 },
             success: function(response) {
                 var card = new Card(response.id, cardName);
                 self.createCard(card);
             }
-           });
+            });
         });
 			
-			// KONSTRUOWANIE ELEMENTU KOLUMNY
-		column.append(columnTitle)
-			.append(columnDelete)
-			.append(columnAddCard)
-			.append(columnCardList);
-			return column;
-		}
-	}
+        // KONSTRUOWANIE ELEMENTU KOLUMNY
+        column.append(columnTitle)
+            .append(columnDelete)
+            .append(columnAddCard)
+            .append(columnCardList);
+        return column;
+    }
+}
+
 Column.prototype = {
     createCard: function(card) {
         this.element.children('ul').append(card.element);
